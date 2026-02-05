6318f484bdf2bb34338b5eef7433762a6780d544
Patricia <patricianjuguna084@gmail.com>
2026-02-05 20:10:56 +0300

    fix: routes in urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import OrderViewSet
 
 router = DefaultRouter()
-router.register(r'orders', OrderViewSet, basename='order')
+router.register("orders", OrderViewSet, basename="orders")
 
-urlpatterns = [path('', include(router.urls))]
+urlpatterns = router.urls
