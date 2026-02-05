46c0bcaed716887e1fc337705eaed27be9f7a756
Patricia <patricianjuguna084@gmail.com>
2026-02-05 20:39:15 +0300

    feat: restrict product management to admins

--- products/views.py
+++ products/views.py
@@ -1,14 +1,10 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
 from .models import Product
-from .serializers import ProductSerializer, ProductCreateSerializer
-from accounts.permissions import IsManager
+from .serializers import ProductSerializer
+from config.permissions import IsAdminOrReadOnly
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
-    permission_classes = [IsAuthenticated, IsManager]
-
-    def get_serializer_class(self):
-        if self.action == "create":
-            return ProductCreateSerializer
-        return ProductSerializer
+    serializer_class = ProductSerializer
+    permission_classes = [IsAdminOrReadOnly]
