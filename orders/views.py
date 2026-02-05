3b7008a5e5e332b674ba65ac5805f4ffc47502be
Patricia <patricianjuguna084@gmail.com>
2026-02-05 20:09:28 +0300

    feat: enable order creation endpoint with write serializer

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,16 @@
-from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from .models import Order
-from .serializers import OrderSerializer
-from accounts.permissions import IsManager
+from .serializers import OrderSerializer, OrderCreateSerializer
 
-class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
-    permission_classes = [IsAuthenticated, IsManager]
+
+class OrderViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return OrderCreateSerializer
+        return OrderSerializer
