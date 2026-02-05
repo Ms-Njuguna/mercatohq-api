ab901d89f6c5ff884cd410366f866180ca13c0f7
Patricia <patricianjuguna084@gmail.com>
2026-02-05 20:26:54 +0300

    feat: implement order viewset with nested create flow

--- orders/views.py
+++ orders/views.py
@@ -1,14 +1,13 @@
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, permissions
 from .models import Order
 from .serializers import OrderSerializer, OrderCreateSerializer
 
 
-class OrderViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+class OrderViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return Order.objects.filter(user=self.request.user)
+        return Order.objects.filter(user=self.request.user).prefetch_related("items")
 
     def get_serializer_class(self):
         if self.action == "create":
