9bf7305c0ef4361b6b7ab126368a52545b022539
Patricia <patricianjuguna084@gmail.com>
2026-02-05 20:40:15 +0300

    feat: restrict inventory access to admins only

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Inventory
+from .serializers import InventorySerializer
+from config.permissions import IsAdmin
 
-# Create your views here.
+
+class InventoryViewSet(viewsets.ModelViewSet):
+    queryset = Inventory.objects.select_related("product")
+    serializer_class = InventorySerializer
+    permission_classes = [IsAdmin]
